bbe96587be6e110ccf00057f579ccb624d111066
nicolefemello <nicfmello@gmail.com>
2025-06-11 15:06:06 -0300

    FEAT: adicionando layout #13

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+from src.layouts.default import layout
+from src.router.index import router
+
 st.set_page_config(
     page_title="Monitoramento Hidrológico", layout="wide", page_icon="🌊"
 )
+
+
+layout(router)  # carrega layout
