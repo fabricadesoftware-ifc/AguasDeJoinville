d99397dfdb4a2a1bb57eb76df0e39c865ca674a3
nicolefemello <nicfmello@gmail.com>
2025-06-11 15:54:49 -0300

    STYLE: add estilos e responsividade fluida #13

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+from src.utils.utils import load_css
 from src.layouts.default import layout
 from src.router.index import router
 
@@ -7,5 +8,6 @@ st.set_page_config(
     page_title="Monitoramento Hidrológico", layout="wide", page_icon="🌊"
 )
 
+load_css("src/assets/main.css")  # carrega css
 
 layout(router)  # carrega layout
